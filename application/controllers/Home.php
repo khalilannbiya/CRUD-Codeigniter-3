bba513ae20c2f2af8979767faca2f04288c41a07
khalilannbiya <syeichkhalil@gmail.com>
2022-04-06 10:53:10 +0700

    update

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,10 +1,10 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class Home extends CI_Controller
 {
   public function index()
   {
-
     $data['title'] = "Home";
     $this->load->view('templates/header', $data);
     $this->load->view('home/index');
